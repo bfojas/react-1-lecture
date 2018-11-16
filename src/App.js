38e5ec1cef153203459aead6e576cfb8cb824770
Bradley Fojas <brad.fojas@yahoo.com>
2018-11-16 10:44:08 -0700

    my boss, tyler wants me to stop being lame

--- src/App.js
+++ src/App.js
@@ -5,22 +5,35 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Title</h1>
+        <ul>  
+          <li>Ross</li>
+          <li>Chandler</li>
+          <li>Monica</li>
+          <li>Phoebe</li>
+          <li>Rachel</li>
+          <li>Joey</li>
+        </ul>
       </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src="https://en.meming.world/images/en/thumb/2/2c/Surprised_Pikachu_HD.jpg/300px-Surprised_Pikachu_HD.jpg" className="App-logo" alt="logo" />
+      //     <p>
+      //       You are a dipshit.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Pretend you learn react
+      //     </a>
+      //   </header>
+      // </div>
+
+    
     );
   }
 }
